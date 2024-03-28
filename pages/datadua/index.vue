868e3dbb687cc8c738982c644894cda96f460852
yannuindra30 <yannuindra@gmail.com>
2024-03-28 15:26:42 +0700

    crudss

--- pages/datadua/index.vue
+++ pages/datadua/index.vue
@@ -76,6 +76,125 @@
       </v-form>
     </v-container>
   </v-card>
+  -
+  <v-data-table
+    :headers="headers"
+    :items="dataUser"
+    :items-per-page="9"
+    :search="search"
+  >
+    <!--judul tabel dan button new item di pojok kanan-->
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Data User RSSA</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          density="compact"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo-filled"
+          flat
+          hide-details
+          single-line
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="600px">
+          <template v-slot:activator="{ props }">
+            <v-btn class="mb-2" color="primary" dark v-bind="props">
+              TAMBAH DATA
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      v-model="editedItem.nip"
+                      label="NIP"
+                    ></v-text-field
+                    ><br />
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      v-model="editedItem.nama_pegawai"
+                      label="Nama Pegawaik"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      v-model="editedItem.rolename"
+                      label="NIP / NIPTTK"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      v-model="editedItem.nama_unit"
+                      label="Departemen"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      v-model="editedItem.nama_grupunit"
+                      label="Aktif"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="close">
+                BATAL
+              </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="save">
+                SIMPAN
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >Apakah anda yakin mau menghapus data ini?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <!--end of v-slot top-->
+    <!--actions kolom edit dan delete -->
+    <template v-slot:item.actions="{ item }">
+      <v-icon class="me-2" size="small" @click="detailItem(item)">
+        mdi-account-details
+      </v-icon>
+      <v-icon class="me-2" size="small" @click="editItem(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+    <!--end of actions-->
+  </v-data-table>
 </template>
 
 <script setup>
@@ -90,6 +209,18 @@ const selectedPerawat = ref();
 const selectedRoles = ref();
 const selectedUnit = ref();
 
+const search = ref();
+const userDetail = ref();
+const dataUser = ref();
+const headers = [
+  { title: "NIP", value: "nip", sortable: true },
+  { title: "Nama Pegawai", value: "nama_pegawai", sortable: true },
+  { title: "Role", value: "rolename", sortable: true },
+  { title: "Nama Unit", value: "nama_unit", sortable: true },
+  { title: "Nama Grup Unit", value: "nama_grupunit", sortable: true },
+  { title: "Actions", key: "actions", sortable: false },
+];
+
 const newUser = ref({
   nip: "",
   nama_pegawai: "",
@@ -99,7 +230,90 @@ const newUser = ref({
   departemen: "",
   nipb: "",
 });
+const dialog = ref(false);
+const dialogDelete = ref(false);
+const editedIndex = ref(-1);
+const editedItem = ref({
+  name: "",
+  calories: 0,
+  fat: 0,
+  carbs: 0,
+  protein: 0,
+});
+const defaultItem = ref({
+  name: "",
+  calories: 0,
+  fat: 0,
+  carbs: 0,
+  protein: 0,
+});
+const formTitle = computed(() => {
+  return editedIndex.value === -1 ? "New Data" : "Edit Data";
+});
+//end of tambahan
 
+const { data: userSimRS } = useFetch("/api/user/userjoin");
+const { data: userDetailData } = useFetch("/api/user/userdetail"); // Assuming userDetail endpoint
+
+//tambahan
+function detailItem(item) {
+  editedIndex.value = userDetail.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialog.value = true;
+}
+
+function editItem(item) {
+  editedIndex.value = dataUser.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialog.value = true;
+}
+function deleteItem(item) {
+  editedIndex.value = dataUser.value.indexOf(item);
+  editedItem.value = Object.assign({}, item);
+  dialogDelete.value = true;
+}
+function deleteItemConfirm() {
+  dataUser.value.splice(editedIndex.value, 1);
+  closeDelete();
+}
+function close() {
+  dialog.value = false;
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem.value);
+    editedIndex.value = -1;
+  });
+}
+function closeDelete() {
+  dialogDelete.value = false;
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem.value);
+    editedIndex.value = -1;
+  });
+}
+function save() {
+  if (editedIndex.value > -1) {
+    Object.assign(dataUser.value[editedIndex.value], editedItem.value);
+  } else {
+    dataUser.value.push(editedItem.value);
+  }
+  close();
+}
+watch(dialog, (val) => {
+  val || close();
+});
+watch(dialogDelete, (val) => {
+  val || closeDelete();
+});
+//end of tambahan
+
+onMounted(async () => {
+  // Await both fetches to ensure data is available before use
+  await userSimRS.value;
+  await userDetailData.value;
+
+  dataUser.value = userSimRS.value; // Assuming userSimRS has user data
+  userDetail.value = userDetailData.value; // Assuming userDetailData has detail data
+});
 const { data: roles } = useFetch("/api/roles");
 const { data: unit } = useFetch("/api/unit");
 const { data: dokter } = useFetch("/api/dokter");
